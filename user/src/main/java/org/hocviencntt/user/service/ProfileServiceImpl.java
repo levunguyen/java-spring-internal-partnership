0419881d898683734c17faec52f05675843a98d1
Administrator <Administrator@DELL>
2016-10-25 21:29:51 +0700

    profile

---
+++ user/src/main/java/org/hocviencntt/user/service/ProfileServiceImpl.java
@@ -0,0 +1,16 @@
+package org.hocviencntt.user.service;
+
+import org.hocviencntt.user.model.Profile;
+import org.hocviencntt.user.repository.ProfileRepository;
+
+public class ProfileServiceImpl implements ProfileService{
+
+	public ProfileRepository profileRepository;
+
+	public Profile loadProfile(String username) {
+		return profileRepository.findByUsername(username);
+	}
+	
+	
+
+}
