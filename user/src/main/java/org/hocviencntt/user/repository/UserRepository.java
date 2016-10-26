394837fcc5ee0bf607f0a57455b7a2537f1dedc6
Administrator <Administrator@DELL.net.fpt>
2016-10-26 23:12:24 +0700

    remove findBylocationandbusiness

--- user/src/main/java/org/hocviencntt/user/repository/UserRepository.java
+++ user/src/main/java/org/hocviencntt/user/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package org.hocviencntt.user.repository;
 
-import java.util.List;
 
 import org.hocviencntt.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+	
 }
