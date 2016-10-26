654387f0d4a061e8ff446996b92fe44e52498f15
Administrator <Administrator@DELL.net.fpt>
2016-10-26 23:09:55 +0700

    delete location business model in user

--- user/src/main/java/org/hocviencntt/user/model/User.java
+++ user/src/main/java/org/hocviencntt/user/model/User.java
@@ -1,33 +1,23 @@
 package org.hocviencntt.user.model;
 
-import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 @Entity
 @Table(name= "user")
 public class User {
 	@Id
-	private String username;
+	private String userName;
 	@Column
 	private String password;
-	@OneToMany(mappedBy="user",fetch=FetchType.EAGER)
-	private Set<Business> listBusiness;
-	@OneToMany(mappedBy="user",fetch=FetchType.EAGER)
-	private Set<Location> listLocation;
-	@OneToOne(mappedBy = "user" ,fetch=FetchType.EAGER)
-	private Profile profile;
 	
 	public String getUsername() {
-		return username;
+		return userName;
 	}
 	public void setUsername(String username) {
-		this.username = username;
+		this.userName = username;
 	}
 	public String getPassword() {
 		return password;
@@ -37,11 +27,11 @@ public class User {
 	}
 	@Override
 	public String toString() {
-		return "User [username=" + username + ", password=" + password + "]";
+		return "User [username=" + userName + ", password=" + password + "]";
 	}
 	public User(String username, String password) {
 		super();
-		this.username = username;
+		this.userName = username;
 		this.password = password;
 	}
 	
