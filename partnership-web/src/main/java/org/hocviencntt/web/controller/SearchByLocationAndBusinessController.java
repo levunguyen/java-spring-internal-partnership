083e0580c3d5292921ee1b64431106e6fd27d6af
tienttitedu <thaithanhtien1995@gmail.com>
2016-10-27 01:11:12 +0700

    location

--- partnership-web/src/main/java/org/hocviencntt/web/controller/SearchByLocationAndBusinessController.java
+++ partnership-web/src/main/java/org/hocviencntt/web/controller/SearchByLocationAndBusinessController.java
@@ -1,13 +1,8 @@
 package org.hocviencntt.web.controller;
-
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class SearchByLocationAndBusinessController {
 
-	public String searchByLocationAndBusiness(){
-		
-		
-		return "suggest-daily.jsp";
-	}
+ 
 }
