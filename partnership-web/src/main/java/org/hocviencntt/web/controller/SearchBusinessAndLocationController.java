65c5b18f747bfbf0e46f8a1a6d2e928cbd5dfd83
tienttitedu <thaithanhtien1995@gmail.com>
2016-10-27 22:45:59 +0700

    delete locationcontroller

--- partnership-web/src/main/java/org/hocviencntt/web/controller/SearchBusinessAndLocationController.java
+++ partnership-web/src/main/java/org/hocviencntt/web/controller/SearchBusinessAndLocationController.java
@@ -1,6 +1,10 @@
 package org.hocviencntt.web.controller;
+import java.util.List;
+
 import org.hocviencntt.business.model.Business;
 import org.hocviencntt.business.service.BusinessService;
+import org.hocviencntt.location.model.Location;
+import org.hocviencntt.location.service.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,11 +13,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class SearchByBusinessController {
+public class SearchBusinessAndLocationController {
 
 	@Autowired
 	BusinessService businessService;
 	
+	@Autowired
+	LocationService locationService;
 	
 	@RequestMapping(value = "/findBusiness", method = RequestMethod.GET)
 	public String findBusiness(@RequestParam("name") String name, Model model) {
@@ -21,5 +27,14 @@ public class SearchByBusinessController {
 		business= businessService.findByBusiness(name);
 		model.addAttribute("business", business);
 		return "suggest-daily";
-	}  
+	} 
+	
+
+	@RequestMapping(value="/findlocation",method=RequestMethod.GET)
+	public String findLocation(Model model){
+		List<Location> listlocations=locationService.findAllLocation();
+		model.addAttribute("allOfLocation",listlocations);
+		return "suggest-daily";
+		
+	}
 }
