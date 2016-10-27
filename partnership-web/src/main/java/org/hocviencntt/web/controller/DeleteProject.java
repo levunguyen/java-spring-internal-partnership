76a7ac46e7a0f9d817dbd26e8fcef7a701ea361e
ASUS <ASUS@192.168.1.10>
2016-10-27 23:32:41 +0700

    deleteproject

--- partnership-web/src/main/java/org/hocviencntt/web/controller/DeleteProject.java
+++ partnership-web/src/main/java/org/hocviencntt/web/controller/DeleteProject.java
@@ -3,7 +3,6 @@ package org.hocviencntt.web.controller;
 import org.hocviencntt.project.service.projectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +11,7 @@ public class DeleteProject {
 	@Autowired
 	projectService project;
 	@RequestMapping(value="/deleteproject",method =RequestMethod.GET)
-	public String deletePrọect(@RequestParam(value="id") int id,Model model){
+	public String deleteProject(@RequestParam(value="id") int id){
 		if (project.deleteShop(id)) {
 			return null;
 		}
