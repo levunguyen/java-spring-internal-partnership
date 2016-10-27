7c767accfadac3b4dddc6a35dd14e6297781d4b4
ASUS <ASUS@192.168.1.10>
2016-10-27 21:11:49 +0700

    project model

--- project/src/main/java/org/hocviencntt/project/model/Project.java
+++ project/src/main/java/org/hocviencntt/project/model/Project.java
@@ -26,4 +26,46 @@ public class Project {
 	private String business;
 	@Column(name="phone")
 	private int phone;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getBusiness() {
+		return business;
+	}
+	public void setBusiness(String business) {
+		this.business = business;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
 }
