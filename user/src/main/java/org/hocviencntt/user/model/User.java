9d31a2d03688e60e220c7a9814ad29accd581716
PC <PC@PHI-LONG-PC>
2016-10-28 07:38:19 +0700

    ha change pom

--- user/src/main/java/org/hocviencntt/user/model/User.java
+++ user/src/main/java/org/hocviencntt/user/model/User.java
@@ -1,23 +1,33 @@
 package org.hocviencntt.user.model;
 
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 @Entity
 @Table(name= "user")
 public class User {
 	@Id
-	private String userName;
+	private String username;
 	@Column
 	private String password;
+	@OneToMany(mappedBy="user",fetch=FetchType.EAGER)
+	private Set<Business> listBusiness;
+	@OneToMany(mappedBy="user",fetch=FetchType.EAGER)
+	private Set<Location> listLocation;
+	@OneToOne(mappedBy = "user" ,fetch=FetchType.EAGER)
+	private Profile profile;
 	
 	public String getUsername() {
-		return userName;
+		return username;
 	}
 	public void setUsername(String username) {
-		this.userName = username;
+		this.username = username;
 	}
 	public String getPassword() {
 		return password;
@@ -27,11 +37,11 @@ public class User {
 	}
 	@Override
 	public String toString() {
-		return "User [username=" + userName + ", password=" + password + "]";
+		return "User [username=" + username + ", password=" + password + "]";
 	}
 	public User(String username, String password) {
 		super();
-		this.userName = username;
+		this.username = username;
 		this.password = password;
 	}
 	
--- user/src/main/java/org/hocviencntt/user/repository/UserRepository.java
+++ user/src/main/java/org/hocviencntt/user/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package org.hocviencntt.user.repository;
 
+import java.util.List;
 
+import org.hocviencntt.user.model.Business;
+import org.hocviencntt.user.model.Location;
 import org.hocviencntt.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-	
+	public List<User>findByLocationAndBusiness(Location location,Business business);
 }
