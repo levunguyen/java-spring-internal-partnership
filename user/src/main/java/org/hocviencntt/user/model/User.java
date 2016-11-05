6246c761f0ea8e99c984ecb25ebbe4d0c098bdc9
PC <PC@PHI-LONG-PC>
2016-11-05 14:48:45 +0700

    ha

--- user/src/main/java/org/hocviencntt/user/model/User.java
+++ user/src/main/java/org/hocviencntt/user/model/User.java
@@ -13,6 +13,8 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import org.hocviencntt.business.model.Business;
+import org.hocviencntt.location.model.Location;
+import org.hocviencntt.project.model.Project;
 @Entity
 @Table(name= "user")
 public class User {
@@ -21,80 +23,84 @@ public class User {
 	@Column(name = "password" )
 	private String passWord;
 	
-	@OneToMany(fetch = FetchType.EAGER,mappedBy = "business" )
+	@OneToMany(fetch = FetchType.EAGER,mappedBy = "user" )
 	private List<Business> business;
 	
 	
-	public List<Business> getBusiness() {
-		return business;
-	}
-
-
-
-	public void setBusiness(List<Business> business) {
-		this.business = business;
-	}
-
-
-
+	@OneToMany(fetch = FetchType.EAGER,mappedBy = "user")
+	private List<Location> location;	
+	
+	@OneToMany(fetch = FetchType.EAGER,mappedBy = "user")
+	private List<Project> project;
+	
 	@OneToOne(mappedBy="user",fetch=FetchType.EAGER)
 	private Profile profile;
-	
-	
-	public User(String userName, String passWord, Profile profile) {
-		super();
-		this.userName = userName;
-		this.passWord = passWord;
-		this.profile = profile;
-	}
-
-
-
-	@Override
-	public String toString() {
-		return "User [userName=" + userName + ", passWord=" + passWord + ", profile=" + profile + "]";
-	}
-
-
 
 	public String getUserName() {
 		return userName;
 	}
 
-
-
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-
-
 
 	public String getPassWord() {
 		return passWord;
 	}
 
-
-
 	public void setPassWord(String passWord) {
 		this.passWord = passWord;
 	}
 
+	public List<Business> getBusiness() {
+		return business;
+	}
 
+	public void setBusiness(List<Business> business) {
+		this.business = business;
+	}
+
+	public List<Location> getLocation() {
+		return location;
+	}
+
+	public void setLocation(List<Location> location) {
+		this.location = location;
+	}
+
+	public List<Project> getProject() {
+		return project;
+	}
+
+	public void setProject(List<Project> project) {
+		this.project = project;
+	}
 
 	public Profile getProfile() {
 		return profile;
 	}
 
-
-
 	public void setProfile(Profile profile) {
 		this.profile = profile;
 	}
 
+	@Override
+	public String toString() {
+		return "User [userName=" + userName + ", passWord=" + passWord + ", business=" + business + ", location="
+				+ location + ", project=" + project + ", profile=" + profile + "]";
+	}
 
-
-	public User(){
-		
+	public User(String userName, String passWord, List<Business> business, List<Location> location,
+			List<Project> project, Profile profile) {
+		super();
+		this.userName = userName;
+		this.passWord = passWord;
+		this.business = business;
+		this.location = location;
+		this.project = project;
+		this.profile = profile;
 	}
 	
+	
+
 }
